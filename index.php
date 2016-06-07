02b36d595f6dd2c84db598ba1ede72f2c09820ba
Ihor Mykhalchenko <Ihor Mykhalchenko>
2016-06-08 01:20:29 +0300

    init

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <?php include 'components/head.php'; ?>
+    </head>
+    <body>
+        <header>
+
+            <section class="light-bg pt2 pb2 cover relative" style="background-image: url(design/images/header.jpg);">
+              <div class="container wide">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h1 class="white">Framework</h1>
+                  </div>
+                  <div class="col-md-6">
+                    <ul class="breadcrumbs white lh1 right-md">
+                      <li><a href="http://mycode.in.ua/">MyCode</a></li>
+                      <li>Framework</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="floating-button">
+                <a href="#" class="btn-fl pink-bg hover-bg-darken ripple" title="Propose additions"><i class="mti">add</i></a>
+              </div>
+            </section>
+        </header>
+        <main>
+
+        </main>
+        <!-- Google Icons -->
+        <!-- https://design.google.com/icons/ -->
+    </body>
+</html>
